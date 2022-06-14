b4b526ad6fd0dea0ad9b070e529629437095e1b2
Noah Meeker <noahmeeker@gmail.com>
2022-06-14 09:13:13 -0700

    Added base calculations

--- scr/SimpleCalc.cpp
+++ scr/SimpleCalc.cpp
@@ -1,42 +1,44 @@
-#include <iotsream>
+#include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-	string op;
-	int num1, num2, ans;
+	char op, q;
+	double num1, num2, ans;
 	bool quit = false;
 	
 	cout << "Welcome To SimpleCalc\n" << endl;
 	cout << "@author: NMeek" << endl;
 	cout << "-----------------------\n" << endl;
-	cout << "Enter equation below\n";
-			"(i.e. 1 + 1)" << endl;
+	cout << "Enter equation below\n (i.e. 1 + 1)" << endl;
 
 	while (!quit) {
-		cin << num1 << op << num2 << endl;
+		cin >> num1 >> op >> num2;
 
 		switch (op){
-			case +:
+			case '+':
 				ans = num1 + num2;
-			case -:
+			case '-':
 				ans = num1 - num2;
-			case *:
+			case '*':
 				ans = num1 * num2;
-			case /:
+			case '/':
 				if (num2 == 0) {
 					cout << "Error: Division by Zero" << endl;
 					break;
 				}
 				ans = num1 / num2;
-			case sqrt:
+			case '$':
 				ans = sqrt(num1);
-			case pow:
-				ans = num1^num2;
+			case '^':
+				ans = pow(num1,num2);
 			default:
+				cout << "Incorrect operator recieved. Please use +,-,*,/,'sqrt','pow'" << endl;
 
-	
+		cout << "Continue? [y]es [n]o:";
+		cin >> q;
+		if (q == 'y') quit = true;
 		}
 	}	
 
