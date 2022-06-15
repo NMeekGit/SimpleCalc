387031078475f77c44ce2cb1c80b385ed6ce4a30
Noah Meeker <noahmeeker@gmail.com>
2022-06-14 20:54:23 -0700

    Reorganized into functions

--- src/SimpleCalc.cpp
+++ src/SimpleCalc.cpp
@@ -3,6 +3,41 @@
 
 using namespace std;
 
+double calculations(double n1, double n2, char op) {
+	double ans;
+
+	switch (op){
+			case '+':
+				ans = n1 + n2;
+				break;
+			case '-':
+				ans = n1 - n2;
+				break;
+			case '*':
+				ans = n1 * n2;
+				break;
+			case '/':
+				if (n2 == 0) {
+					cout << "Error: Division by Zero" << endl;
+					break;
+				}
+				ans = n1 / n2;
+				break;
+			case '$':
+				ans = sqrt(n1);
+				break;
+			case '^':
+				ans = pow(n1,n2);
+				break;
+			default:
+				cout << "Incorrect operator recieved. Please use +,-,*,/,'sqrt','pow'" << endl;
+				break;
+		}
+	return ans;
+}
+
+
+
 int main() {
 	char op, q;
 	double num1, num2, ans;
@@ -14,35 +49,10 @@ int main() {
 	cout << "Enter equation below\n (i.e. 1 + 1)" << endl;
 
 	while (!quit) {
-		cin >> num1 >> op >> num2;
+		cin >> num1 >> op;
+		if (op != '$') cin >> num2; // Case for sqrt.
 
-		switch (op){
-			case '+':
-				ans = num1 + num2;
-				break;
-			case '-':
-				ans = num1 - num2;
-				break;
-			case '*':
-				ans = num1 * num2;
-				break;
-			case '/':
-				if (num2 == 0) {
-					cout << "Error: Division by Zero" << endl;
-					break;
-				}
-				ans = num1 / num2;
-				break;
-			case '$':
-				ans = sqrt(num1);
-				break;
-			case '^':
-				ans = pow(num1,num2);
-				break;
-			default:
-				cout << "Incorrect operator recieved. Please use +,-,*,/,'sqrt','pow'" << endl;
-				break;
-		}
+		ans = calculations(num1, num2, op);		
 		cout << "Answer: " << ans << endl;
 
 		cout << "Continue? [y]es [n]o:";
@@ -52,3 +62,4 @@ int main() {
 
 	return 0;
 }
+
